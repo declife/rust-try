8a9085215704b838eeab4a949db87a21fb560d5e
liuxl <liuxl@investandpay.com>
2021-03-19 16:22:47 +0800

    add:some data type

--- idea/src/main.rs
+++ idea/src/main.rs
@@ -1,10 +1,27 @@
+mod data_type;
+
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
 
-    println!("Guess the number!");
+    let rec = Rectangle {
+        width: 23,
+        height: 24
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_by_rec(&rec)
+    );
+
+    println!(
+        "Rectangle is {:#?}",
+        rec
+    );
+
+    println!("Guess the number! {}", rec.height);
 
     println!("Please input your guess:");
 
@@ -35,6 +52,43 @@ fn main() {
 }
 
 
+#[derive(Debug)]
+struct Rectangle {
+    width: i32,
+    height: i32,
 
+}
 
+impl Rectangle {
 
+    fn area(&self) -> u32 {
+        (self.width * self.height) as u32
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+fn area(dimensions: (i32, i32)) -> i32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_by_rec(rec: &Rectangle) -> u32 {
+    (rec.height * rec.width) as u32
+}
+
+enum Test {
+    OK,
+    ERR,
+}
+
+impl Test {
+
+    fn handle() {
+
+    }
+
+    fn hand_(&self) {
+
+    }
+}
