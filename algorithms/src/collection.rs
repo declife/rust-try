aab4579c16178349c676dadf0a72ba51e56e7cd8
liuxl <liuxl@investandpay.com>
2021-04-06 15:53:33 +0800

    add: new module

--- algorithms/src/collection.rs
+++ algorithms/src/collection.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
+
 /// 集合：string
 pub fn string_dis() {
     let mut s = String::new();
     s.push('d');
     let _data = "initial contents";
-    println!("{}", String::from("konnichiwa"));
+    let s3 = String::from("lol");
+    let s = format!("{}-{}-{}", s, _data, s3);
+    println!("{}::{}", String::from("konnichiwa"), s);
+}
+
+
+/// 集合： HashMap
+pub fn map_foo() {
+   let mut foo_map = HashMap::new();
+    foo_map.insert(String::from("foo"), String::from("bar"));
 }
